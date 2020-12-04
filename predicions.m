f1aaa220212097ec166c23400a487e46adafc031
MisterBigThor <correal.ramos@gmail.com>
2020-12-04 18:55:18 +0100

    script predicions

---
+++ predicions.m
@@ -0,0 +1,20 @@
+
+prompt= 'Entra el nom de la imatge a predir: ';
+nameFile=input(prompt, 's');
+
+try
+    
+    %leer la imagen
+    imread(nameFile);
+    %cargar el modelo de  modelX.mat
+    %load modelX.mat
+    
+    %predecir y dar resultado:
+    class = 1; %cambiar por la funcion de predecir del modelo
+    
+    fprintf('Classe : %s\n', numberToName(class));
+catch
+    fprintf('No existeix el archiu %s \n', nameFile);
+   
+end
+
