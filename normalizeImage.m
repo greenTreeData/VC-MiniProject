e86d796ca332a33f06132cf466bc37397c6c4b04
Marcel <marcel.dfga@gmail.com>
2021-01-10 18:44:14 +0100

    Añade la binarización mediante global treshold, preProcesing.m lo incluye

--- normalizeImage.m
+++ normalizeImage.m
@@ -1,6 +1,4 @@
-function normalized = untitled(i)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function normalized = normalizeImage(i)
 a = min(i(:));  % a: buscar el min. valor dels pixels en totes les columnes
 c = min(a);     % c: buscar el min. value dels pixels en la image
 b = max(i(:));  % b: buscar el max. valor dels pixels en totes les columnes
