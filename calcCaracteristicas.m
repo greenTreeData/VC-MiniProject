fc7bc48a0a963f05487b32a805935cf378aaee9e
Marcel <marcel.dfga@gmail.com>
2021-01-12 17:49:38 +0100

    Mejora preprocesing comienza calcaracteristiques

--- calcCaracteristicas.m
+++ calcCaracteristicas.m
@@ -1,3 +1,27 @@
 function tableRow = calcCaracteristicas(lable, img)
-    tableRow = table(lable, 1,2,3);
+% !!ATENCIÓN!! , si la imagen llega preprocessada -> viene en formato BW i
+% i calcColors no puede funcionar
+
+    %colors
+    
+    [iRed, iBlue, iYellow, iWhite, iBlack] = calcColors(img);
+    
+    %circularitat
+    BW = preProcesing(img); % retorna la imatge en BW
+    
+    stats = regionprops("table", BW,"all");
+    [~,maxidx] = max(stats.Area);
+    circularidad = stats.Circularity(maxidx);
+    
+    %hogs - necessita una extensió: Visual Computation 
+    [~,hogVisualization] = extractHOGFeatures(rgbNorm);
+    
+    %Paraleles
+    paraleles = calcParaleles(img);
+
+    %ocr
+    txt = ocr(img);
+    
+    %fer taula
+    tableRow = table(lable, iRed, iBlue, iYellow, iWhite, iBlack, circularidad, hogVisualization, paraleles, txt);
 end
--- preProcesing.m
+++ preProcesing.m
@@ -1,13 +1,12 @@
 %Donada una imatge, la retornem preprocesada
 function data = preProcesing(data)
     targetSize = [100,200]; %funcion de ejemplo, de momento.
-    data = imresize(data, targetSize);
-    
-    %equalització
+    %data = imresize(data, targetSize);
+  
     dataNormalized = normalizeImage(data);
     normalize      = rgb2gray(dataNormalized);
     level          = graythresh(normalize);
-    %BW             = imbinarize(normalize,level);
-    %data           = BW;
-    data           = normalize;
+    BW             = imbinarize(normalize,level);
+    data           = BW;
+   % data           = dataNormalized;
 end
