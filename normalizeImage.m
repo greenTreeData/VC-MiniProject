b6f21957dc1e7c07999beb19041e3ac4903d8b39
celo4ever <38758762+celo4ever@users.noreply.github.com>
2021-01-12 17:56:25 +0100

    Update normalizeImage.m
    Mejora normalizar

--- normalizeImage.m
+++ normalizeImage.m
@@ -1,9 +1,18 @@
 function normalized = normalizeImage(i)
-a = min(i(:));  % a: buscar el min. valor dels pixels en totes les columnes
-c = min(a);     % c: buscar el min. value dels pixels en la image
-b = max(i(:));  % b: buscar el max. valor dels pixels en totes les columnes
-d = max(b);     % d: buscar el min. value dels pixels en la image
-m = 255/(d - c);  % find the slope of line joining point (0,255) to (rmin,rmax)
-normalized = (i - c) * m; % transform the image according to new slope
-end
 
+  [files, cols, ~] = size(i);
+  im = double(i);
+  sum = im(:,:,1) + im(:,:,2) + im(:,:,3);
+  r = uint8(zeros(files, cols, 3));
+  r(:,:,1) = uint8(255*im(:,:,1)./sum);
+  r(:,:,2) = uint8(255*im(:,:,2)./sum);
+  r(:,:,3) = uint8(255*im(:,:,3)./sum);
+
+  a = min(r(:));  % a: buscar el min. valor dels pixels en totes les columnes
+  c = min(a);     % c: buscar el min. value dels pixels en la image
+  b = max(r(:));  % b: buscar el max. valor dels pixels en totes les columnes
+  d = max(b);     % d: buscar el min. value dels pixels en la image
+  m = 255/(d - c);  % find the slope of line joining point (0,255) to (rmin,rmax)
+  normalized = (r - c) * m; % transform the image according to new slope
+
+end
