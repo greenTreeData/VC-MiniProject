317b6a4a406848de48ba31a296b9e6d345385ba0
Marcel <marcel.dfga@gmail.com>
2021-01-13 17:28:37 +0100

    Añade dos campos de retorno a preProcessing

--- preProcesing.m
+++ preProcesing.m
@@ -1,12 +1,7 @@
 %Donada una imatge, la retornem preprocesada
-function data = preProcesing(data)
-    targetSize = [100,200]; %funcion de ejemplo, de momento.
-    %data = imresize(data, targetSize);
-  
+function [dataNormalized, BW] = preProcesing(data)  
     dataNormalized = normalizeImage(data);
     normalize      = rgb2gray(dataNormalized);
     level          = graythresh(normalize);
     BW             = imbinarize(normalize,level);
-    data           = BW;
-   % data           = dataNormalized;
 end
